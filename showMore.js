b32d9da39904f13ec1aa03fafcce9b03ff61292f
Caleb Alexander <alexander1408@live.missouristate.edu>
2021-05-04 18:58:55 -0500

    Added some quotes and modified jQuery aesthetics

--- showMore.js
+++ showMore.js
@@ -4,9 +4,11 @@ $(document).ready(function(){
         var index = $(".showmore").index(this);
         if ($('.infoModule').eq(index).is(':visible')){
             $('.showmore').eq(index).html("Click here for more.");
+            $('.proverb').eq(index).css("margin-bottom", "revert");
         }
         else {
             $('.showmore').eq(index).html("Click here for less.");
+            $('.proverb').eq(index).css("margin-bottom", "0");
         }
         
         $('.infoModule').eq(index).fadeToggle(400);
